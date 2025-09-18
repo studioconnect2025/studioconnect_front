fda71c6a81f6c7f1c0577800063c49e7f266cc86
Daiana Lopez <Daiana_Celeste_Lopez@outlook.com>
2025-09-18 20:24:00 -0300

    fix build

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import Search from "@/components/search/search";
 
-export default function MyStudioPage() {
-  return <Search/>;
+export default function SearchPage() {
+  return (
+    <Suspense fallback={<p>Cargando búsqueda...</p>}>
+      <Search />
+    </Suspense>
+  );
 }
