e098ea1b0ccd3655960aff78c2ad5600c78b5131
vickygallo <vickievike@hotmail.com>
2025-09-24 20:54:16 -0300

    Login y registro

--- app/register-google/RegisterGoogleHandler.tsx
+++ app/register-google/RegisterGoogleHandler.tsx
@@ -29,7 +29,7 @@ export default function RegisterGoogleHandler() {
 
     try {
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/auth/register/google`,
+        `${process.env.NEXT_PUBLIC_API_URL}auth/register/google`,
         {
           method: "POST",
           headers: {
